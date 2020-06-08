8cd39eaac6ff2fdcfc373bf657d7c65a731cc650
Albarin97 <albar00@hotmail.com>
2020-06-08 15:38:27 -0500

    Reportes Funcionando
    Funcion correcta de generacion de reportes

--- repos.py
+++ repos.py
@@ -1,22 +1,22 @@
 import itertools
 from random import randint
-import webbrowser as wb
 from statistics import mean
-from reportlab.lib.pagesizes import *
-from reportlab.lib.fonts import *
+from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
 import conexion as con
+import webbrowser as wb
 
 def grouper(iterable, n):
     args = [iter(iterable)] * n
-    return itertools.zip_longest(args)
-def export_to_pdf(nombre):
+    return itertools.zip_longest(*args)
+def export_to_pdf():
+
+    data = [("ID Venta", "Marca", "Modelo", "Tipo", "Cliente", "Cantidad", "Costo")]
     registros = con.DataBase().select_pedidos()
-    #print(registros)
-    data = [("idventa", "marca", "modelo", "tipo", "cliente", "cantidad", "costo")]
     for row in registros:
         data.append((str(row[0]),str(row[1]),str(row[2]),str(row[3]),str(row[4]),str(row[5]),str(row[6])))
-    c = canvas.Canvas("reportes/"+nombre, pagesize=A4)
+
+    c = canvas.Canvas("reportes/ventas.pdf", pagesize=A4)
     text=c.beginText(10,810,None)
     text.setFont("Times-Roman", 12)
     text.textLine("ALBAR'S MOTO SPORT: --REPORTE DE VENTAS--")
@@ -28,11 +28,10 @@ def export_to_pdf(nombre):
     y_offset = 50
     # Space between rows.
     padding = 15
-    #ipadding = 60
-
-    xlist = [x + x_offset for x in [0, 200, 250, 300, 350, 400, 480]]
+    
+    xlist = [x + x_offset for x in [0, 60, 200, 250, 300, 350, 400, 480]]
     ylist = [h - y_offset - i*padding for i in range(max_rows_per_page + 1)]
-
+    
     for rows in grouper(data, max_rows_per_page):
         rows = tuple(filter(bool, rows))
         c.grid(xlist, ylist[:len(rows) + 1])
@@ -40,8 +39,7 @@ def export_to_pdf(nombre):
             for x, cell in zip(xlist, row):
                 c.drawString(x + 2, y - padding + 3, str(cell))
         c.showPage()
-
     c.save()
-    wb.open_new('C:/Users/albar/Desktop/ProyectoLogicaFuncional/ProyectoFinalLogicaFuncional/'+nombre)
+    wb.open_new('C:/Users/albar/Desktop/ProyectoLogicaFuncional/reportes/ventas.pdf')
 
-export_to_pdf("test.pdf")
+export_to_pdf()
